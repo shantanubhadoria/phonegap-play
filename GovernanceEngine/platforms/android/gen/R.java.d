c847a75694944ec017124d54282724f2b0d5f867
Shantanu Bhadoria <shantanu@cpan.org>
2014-02-25 20:57:07 +0800

    slide

--- GovernanceEngine/platforms/android/gen/R.java.d
+++ GovernanceEngine/platforms/android/gen/R.java.d
@@ -1,9 +1,9 @@
 /home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/gen/com/shantanubhadoria/governanceengine/R.java \
- : /home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable-hdpi/icon.png \
-/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable-xhdpi/icon.png \
+ : /home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable/icon.png \
+/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable-hdpi/icon.png \
 /home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/xml/config.xml \
-/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable-ldpi/icon.png \
-/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/values/strings.xml \
-/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable/icon.png \
 /home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable-mdpi/icon.png \
+/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/values/strings.xml \
+/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable-xhdpi/icon.png \
+/home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/res/drawable-ldpi/icon.png \
 /home/shantanu/github/phonegap-play/GovernanceEngine/platforms/android/bin/AndroidManifest.xml \
